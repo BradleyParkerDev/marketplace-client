6ef7fc7230472771eb191c5cc32a36e9a2bd3302
Bradley Parker <Bradleyscottparker@outlook.com>
2023-04-23 02:29:39 -0500

    updated navbar, input and buttons change if user is logged in

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -22,104 +22,26 @@ const NavBar = (props) => {
 
     const auth = useAuth(); //access the authentication context 
     const navigate = useNavigate();
-
+    const isLoggedIn = auth.userToken;
+    console.log(auth.userToken)
     const {categoriesList} = props;
     const {shouldRefresh} = props
     const {searchCategory} = props
     const {setSearchCategory} = props
     const {searchCategoryName} = props
     const {setSearchCategoryName} = props
-    // console.log(categoriesList)
 
-    function ShowLogoutButton(){
+
+   // Shows Login input when not logged in
+   function ShowLoginInput(){
         return(
-                <Button onClick={()=>{
-                    auth.logout()
-                }}>Logout</Button> 
-        )
-
-    }
-
-    function ShowLoginRegistration(){
-        return <RegistrationModal />
-    }
-
-    function LoggedIn() {
-        const isLoggedIn = auth.userToken;
-        if (isLoggedIn) {
-          return <ShowLogoutButton/>;
-        }
-        return <ShowLoginRegistration/>;
-    }
-
-
-
-
-    function CategoryDivs(props){
-        return(
-            categoriesList.map(category=>(
-                <div className="categoryMenuCard"
-                onClick={()=>{
-                    setSearchCategory(`${category._id}`)
-                    setSearchCategoryName(`${category.name}`)
-                    console.log(searchCategory)
-                }}
-
-                >
-                    <div className="categoryPhoto">
-                        <img 
-                            src={`/css/Icons/${category.name}.png`}
-                            alt={`${category.name}`}
-                        
-                        />
-                    </div>
-                    <div className="categoryName">
-                        <p>{category.name}</p>
-                    </div>
-
-                </div>
-                )
-            )         
-        )
-    }
-
-    // useEffect(()=> {
-    //     //Get Listings
-    //     axios.get(`${urlEndPoint}/listings/get-listings-by-category/${searchCategory}`)
-    //     .then(function (response){
-    //       console.log(response.data);
-    //         setListingCategorySearch("")
-    //         setListingCategorySearch(response.data.listings);
-    //         console.log(listingCategorySearch);
-    
-    //     })
-    //     .catch(function (error){
-    //         console.log(error);
-    //     })
-    //     .finally(function (){
-    //       //always executed
-    //     })
-    // },[])
-    
-
-    return(
-        <div id = "navDiv">
-            <h1 id="logo" onClick={()=>{
-                    setSearchCategory("All Listings")
-                    setSearchCategoryName("All Listings")
-                    console.log(searchCategory)
-                    navigate("/") }}>
-                Parker's Market</h1>
-            {/* divider */}
-            <hr class="solid"/> 
-            <h3>{auth.userEmail && `Hi, ${auth.userEmail}`}</h3>
             <div id = "login">
+                {/*  Email Text Input */}
                 <p id="loginMessage">(Login to create a listing)</p>
                 <input 
                 id="loginEmail"
                 class="form-control"
-
-                type = "text" 
+                type = "email" 
                 name = "email"
                 placeholder = "Email"
                 autocomplete = "off"
@@ -128,7 +50,7 @@ const NavBar = (props) => {
                 }}
                 /> 
 
-                <br/> 
+                {/* Password Text Input */}
                 <input 
                 id="loginPassword"
                 class="form-control"
@@ -140,6 +62,8 @@ const NavBar = (props) => {
                     setPassword(e.target.value);
                 }}
                 /> 
+
+                {/* Login Button */}
                 <Button 
                     id="loginButton" 
                     variant="primary"
@@ -148,38 +72,123 @@ const NavBar = (props) => {
                         const loginResult = await auth.login(email, password);
                         console.log("button onclick loginResult: ", loginResult)
                         if (loginResult.success) {
-                          navigate("/")
+                        navigate("/")
                         }
                         if (!loginResult.success) {
-                          setLoginMessage(loginResult.message)
+                        setLoginMessage(loginResult.message)
                         }
-                      }}
+                    }}
                     >
                     Login
-                </Button>
-                <br/>
-                <RegistrationModal />
-                <Button id="createButton" onClick={()=>{
-                    navigate("/listings/create-listing")
-                    }}>
-                    + Create New Listing
-                </Button>
-                <Button id="logoutButton" variant="secondary" onClick={()=>{
-                    auth.logout()
-                }}>Logout</Button> 
+                </Button>  
             </div>
+        )
+    }
+
+    // Shows create listing and logout buttons when logged in
+    function ShowCreateListingLogoutButtons(){
+        return(
+            <>
+            <h3>{auth.userEmail && `Hi, ${auth.userEmail}`}</h3>
+            <Button id="createButton" onClick={()=>{
+                navigate("/listings/create-listing")
+                }}>
+                + Create New Listing
+            </Button>
+
+            <Button 
+            id="logoutButton"
+            variant="secondary"
+            onClick={()=>{
+            auth.logout()
+            navigate("/")
+            }}>
+                Logout
+            </Button>             
+            
+            </>
+
+        )
+
+    }
+
+ 
+    // Shows registration button
+    function ShowRegistration(){
+        return <RegistrationModal />
+    }
+
+
+
+    //Creates cards in the navbar for each category
+    function CategoryDivs(props){
+        return(
+            categoriesList.map(category=>(
+                <div className="categoryMenuCard"
+                onClick={()=>{
+                    setSearchCategory(`${category._id}`)
+                    setSearchCategoryName(`${category.name}`)
+                    console.log(searchCategory)
+                }}
+                >
+                    <div className="categoryPhoto">
+                        <img 
+                            src={`/css/Icons/${category.name}.png`}
+                            alt={`${category.name}`}
+                        />
+                    </div>
+                    <div className="categoryName">
+                        <p>{category.name}</p>
+                    </div>
+                </div>
+                )
+            )         
+        )
+    }
+
+
+    return(
+        <div id = "navDiv">
+            <h1 id="logo" onClick={()=>{
+                    setSearchCategory("All Listings")
+                    setSearchCategoryName("All Listings")
+                    console.log(searchCategory)
+                    navigate("/") }}>
+                Parker's Market
+            </h1>
             {/* divider */}
             <hr class="solid"/> 
 
+            {isLoggedIn == null && ShowLoginInput()}
+            {isLoggedIn == null && ShowRegistration()}
+            {isLoggedIn != null && ShowCreateListingLogoutButtons()}
+
+
+            {/* divider */}
+            <hr class="solid"/> 
             <div id = "categories">
                 <h3>Categories</h3>
                 {categoriesList.length > 0 && CategoryDivs()}
-
             </div>
-
-
         </div>
     )
 }
 export default NavBar;
 
+
+// useEffect(()=> {
+//     //Get Listings
+//     axios.get(`${urlEndPoint}/listings/get-listings-by-category/${searchCategory}`)
+//     .then(function (response){
+//       console.log(response.data);
+//         setListingCategorySearch("")
+//         setListingCategorySearch(response.data.listings);
+//         console.log(listingCategorySearch);
+//     })
+//     .catch(function (error){
+//         console.log(error);
+//     })
+//     .finally(function (){
+//       //always executed
+//     })
+// },[])
